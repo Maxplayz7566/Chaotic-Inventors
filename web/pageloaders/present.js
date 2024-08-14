4a6b8c0aa425a85b9f3174640d98bd46ecb8f9fe
Maxplayz7566 <maxplayz7566yt@gmail.com>
2024-08-14 13:45:35 -0600

    Add files via upload

---
+++ web/pageloaders/present.js
@@ -0,0 +1,32 @@
+function loadpresentpage() {
+    document.body.innerHTML = "";
+    const body = document.body;
+    
+    
+    var label = document.createElement("h1")
+    label.id = "suwigaiow";
+    label.innerText = "You're presenting, look at the screen!";
+    label.style.display = 'inline-block';
+    label.style.fontSize = '36px';
+    label.style.animation = 'rotate 3s infinite ease-in-out';
+    
+    const style = document.createElement('style');
+    style.textContent = `
+        @keyframes rotate {
+            0% {
+                transform: rotate(-5deg);
+            }
+            50% {
+                transform: rotate(5deg);
+            }
+            100% {
+                transform: rotate(-5deg);
+            }
+        }
+    `;
+    document.head.appendChild(style);
+    
+    body.appendChild(label);
+    
+    return label;
+}

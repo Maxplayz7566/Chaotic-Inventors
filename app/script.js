3149be7b69a3f3496f84be7035f4d4ca57c8e341
Maxplayz7566 <maxplayz7566yt@gmail.com>
2024-08-14 13:34:33 -0600

    Add files via upload

---
+++ app/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("keydown", function (event) {
+    if (event.ctrlKey && event.key === "r") {
+        event.preventDefault();
+
+        window.location.reload();
+    }
+
+    if (event.key === "F11") {
+        console.log("asdu");
+        event.preventDefault();
+        pywebview.api.fullscreen();
+    }
+});
